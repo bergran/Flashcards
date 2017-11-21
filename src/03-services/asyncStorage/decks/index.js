b797e7a563316acbac2167721d3da6ca6da35118
a.berho.medlab <a.berho@medlabmg.com>
2017-11-21 15:55:51 +0100

    add: Added asynStorage objects

---
+++ src/03-services/asyncStorage/decks/index.js
@@ -0,0 +1,8 @@
+import { AsyncStorage } from 'react-native'
+import { DECK } from 'constants/deck/constants'
+
+export const getDecks = () => AsyncStorage.getItem(DECK)
+
+export const createDeck = deck => AsyncStorage.setItem(DECK, deck)
+
+export const mergeDeck = deck => AsyncStorage.mergeItem(DECK, deck)
