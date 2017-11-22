53c739058059a0f011c7376329799ff10c352549
a.berho.medlab <a.berho@medlabmg.com>
2017-11-22 10:47:12 +0100

    add: Added thunk middleware to store

--- src/06-store/store.js
+++ src/06-store/store.js
@@ -1,5 +1,8 @@
-import { createStore } from 'redux'
+import { createStore, applyMiddleware, compose } from 'redux'
+import thunk from 'redux-thunk'
 
-export default function createStoreCustom (reducer, initialState, middleware) {
-    return createStore(reducer, initialState, middleware)
+export default function createStoreCustom (reducer = null, initialState = null, middleware = null) {
+    return createStore(reducer, initialState, compose(applyMiddleware(
+        thunk
+    )))
 }
