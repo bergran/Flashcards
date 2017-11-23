ada8d84f51241461894a32c4a58d79dd50d0d52a
a.berho.medlab <a.berho@medlabmg.com>
2017-11-23 16:58:02 +0100

    refactor: FlashCardStatusBar can customize backgroundColor

--- src/01-containers/App/FlashCardStatusBar.js
+++ src/01-containers/App/FlashCardStatusBar.js
@@ -1,11 +1,16 @@
 import React, { PureComponent } from 'react'
-import { StatusBar, StyleSheet, View } from 'react-native'
+import { StatusBar, View } from 'react-native'
 import { Constants } from 'expo'
 
 export default class FlashCardStatusBar extends PureComponent {
+    static defaultProps = {
+        backgroundColor: '#2067d8'
+    }
+
     render () {
+        const { backgroundColor } = this.props
         return (
-            <View style={{backgroundColor: '#2067d8', height: Constants.statusBarHeight}}>
+            <View style={{backgroundColor , height: Constants.statusBarHeight}}>
                 <StatusBar
                     translucent={true}
                     barStyle={'light-content'}
