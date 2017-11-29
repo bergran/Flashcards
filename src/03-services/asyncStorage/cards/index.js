849bce208277ce1577877015dd64120f62e077b4
Ángel Berhó <bergraan@gmail.com>
2017-11-29 21:02:00 +0100

    fix: addCard asyncstorage

--- src/03-services/asyncStorage/cards/index.js
+++ src/03-services/asyncStorage/cards/index.js
@@ -3,6 +3,6 @@ import { CARD } from '../../../../constants/cards/constants'
 
 export const getCards = () => AsyncStorage.getItem(CARD).then(cards => JSON.parse(cards))
 
-export const createCard = card => AsyncStorage.setItem(CARD, JSON.stringify(card))
+export const createCard = card => AsyncStorage.mergeItem(CARD, JSON.stringify(card))
 
 export const mergeCard = card => AsyncStorage.mergeItem(CARD, JSON.stringify(card))
