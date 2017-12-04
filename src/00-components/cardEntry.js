8fe20f140b244d7dadab243d349e8150ec7a88f5
Ángel Berhó <bergraan@gmail.com>
2017-12-04 18:13:40 +0100

    feat: show all cards of deck to remove it

---
+++ src/00-components/cardEntry.js
@@ -0,0 +1,69 @@
+import React, { PureComponent } from 'react'
+import Swipeout from 'react-native-swipeout'
+import { Button, Platform, StyleSheet, TouchableOpacity, Text, ScrollView, View } from 'react-native'
+
+export default class CardEntry extends PureComponent {
+    static defaultProps = {
+        onDelete: () => null
+    }
+
+    render () {
+        const { card, width } = this.props
+
+        return (
+            <View style={[styles.container]}>
+                <ScrollView
+                    showsHorizontalScrollIndicator={false}
+                    horizontal
+                    ref={'scroll'}
+                    onMomentumScrollEnd={this.handleEndScroll}
+                >
+                    <View style={[{width: width + 20}, styles.card]}>
+                        <Text>{`Card: ${card.key + 1}`}</Text>
+                        <Text>{`Question: ${card.question}`}</Text>
+                        <Text>{`Answer: ${card.answer}`}</Text>
+                    </View>
+                    <View style={styles.button}>
+                        <Text>Remove</Text>
+                    </View>
+                </ScrollView>
+            </View>
+        )
+    }
+
+    handleEndScroll = e => {
+        if (e.nativeEvent.contentOffset.x === 100) {
+            this.props.onDelete(this.props.card.cardId)
+        }
+        this.refs.scroll.scrollTo({y: 0, x: 0})
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+        backgroundColor: '#FFF',
+        borderRadius: Platform.OS === 'ios' ? 16 : 2,
+        marginTop: 17,
+        shadowRadius: 3,
+        shadowOpacity: 0.8,
+        shadowColor: 'rgba(0, 0, 0, 0.24)',
+        shadowOffset: {
+            width: 0,
+            height: 3
+        }
+    },
+    card: {
+        height: 150,
+        padding: 15,
+        flex: 1,
+        justifyContent: 'space-around'
+    },
+    button: {
+        height: 150,
+        width: 100,
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: Platform.OS === 'ios' ? '#FFF' : 'red'
+    }
+})
--- src/01-containers/Cards/showCards/index.js
+++ src/01-containers/Cards/showCards/index.js
@@ -1,27 +1,68 @@
 import React, { PureComponent } from 'react'
-import { FlatList, Text, View } from 'react-native'
+import { FlatList, StyleSheet, Text, View } from 'react-native'
 import { connect } from 'react-redux'
+import CardEntry from '../../../00-components/cardEntry'
+import { removeCard } from '../../../02-actions/cards/cardActions'
 
 const mapStateToProps = (state, ownProps) => {
     const { navigation } = ownProps
     const { deckId } = navigation.state.params
     return {
-        cards: Object.keys(state.card).filter(cardId => state.card[cardId].deckId === deckId)
-            .map(cardId => Object.assign({}, state.card[cardId], {cardId}))
+        cards: Object.keys(state.card).filter(cardId =>
+            !state.card[cardId].deleted && state.card[cardId].deckId === deckId)
+            .map((cardId, index) => Object.assign({}, state.card[cardId], {cardId, key: index}))
     }
 }
 
-@connect(mapStateToProps)
+@connect(mapStateToProps, {
+    removeCard
+})
 export default class ShowCards extends PureComponent {
     static navigationOptions = ({ navigation }) => ({
         title: `Editing ${navigation.state.params.deckTitle}`
     })
 
     render () {
+        const { cards } = this.props
         return (
-            <View>
-                <Text>Hello world</Text>
+            <View style={styles.container}>
+                <Text>Swipe left to remove card</Text>
+                <FlatList
+                    data={cards}
+                    renderItem={(card, index) => {
+                        return (
+                            <CardEntry
+                                card={card.item}
+                                key={index}
+                                width={300}
+                                onDelete={this.handleDelete}
+                            />
+                        )
+                    }}
+                />
             </View>
         )
     }
+
+    handleDelete = cardId => {
+        console.log('entro')
+        const { cards, removeCard } = this.props
+        const card = cards.filter(cardRaw => cardRaw.cardId === cardId).map(cardRaw => {
+            return {
+                [cardId]: {
+                    deckId: cardRaw.deckId,
+                    question: cardRaw.question,
+                    answer: cardRaw.answer,
+                    deleted: true
+                }
+            }
+        })[0]
+        removeCard(card)
+    }
 }
+
+const styles = StyleSheet.create({
+    container: {
+        padding: 20
+    }
+})
