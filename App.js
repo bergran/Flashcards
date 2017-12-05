ec267431cc449bcc3d889b2bf5338921398a626b
Ángel Berhó <bergraan@gmail.com>
2017-12-05 20:53:45 +0100

    feat: check notification and setup if not exist

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
-import { ActivityIndicator ,StyleSheet, Text, View } from 'react-native'
+import { ActivityIndicator ,StyleSheet, View } from 'react-native'
+import {  Permissions, Notifications } from 'expo'
 import { Provider } from 'react-redux'
 import reducers from './src/05-reducers/index'
 import createStoreCustom from './src/06-store/store'
@@ -7,7 +8,9 @@ import Navigation from './src/01-containers/App/navigation'
 import { getDecks } from "./src/03-services/asyncStorage/decks/index";
 import { getCards } from "./src/03-services/asyncStorage/cards/index";
 import { getQuizzes } from "./src/03-services/asyncStorage/quiz/index";
+import { getNotification, setNotification } from "./src/03-services/asyncStorage/App/notifications";
 import formInitialState from './src/05-reducers/initialStates/form'
+import { createNotificacion } from "./utils/tools";
 
 export default class App extends Component {
     state = {
@@ -19,7 +22,9 @@ export default class App extends Component {
         Promise.all([
             getDecks(),
             getCards(),
-            getQuizzes()
+            getQuizzes(),
+            Permissions.getAsync(Permissions.NOTIFICATIONS),
+            getNotification()
         ]).then(catalogs => {
             const store = createStoreCustom(reducers, {
                     form: formInitialState,
@@ -27,6 +32,39 @@ export default class App extends Component {
                     card: catalogs[1] || {},
                     quiz: catalogs[2] || {}
                 })
+            const notification = catalogs[3]
+            if (notification.status === 'granted') {
+                debugger
+                const dataNotification = catalogs[4]
+                const dateDiff = dataNotification && Date.now() - dataNotification.date
+                if (!dataNotification || (dataNotification && dateDiff > 1800  && !dataNotification.quizDone)) {
+                    // const date = ((new Date()).getTime() / 1000) + 1800
+                    let date = new Date()
+                    date.setMinutes(date.getMinutes() + 30)
+                    date.setSeconds(0)
+                    const notification = createNotificacion('DeckSwift', 'Hey, do you forget to play a quiz? :(')
+                    Notifications.scheduleLocalNotificationAsync(notification, {
+                        time: date,
+                        repeat: 'day'
+                    })
+                        .then(data => setNotification({date: date.getTime(), quizDone: false}))
+                } else if (dateDiff > 86400 && dataNotification.quizDone) {
+                    // const date = ((new Date()).getTime() / 1000) + 1800
+                    Notifications.cancelAllScheduledNotificationsAsync()
+                        .then(data => {
+                            let date = new Date()
+                            date.setMinutes(date.getMinutes() + 30)
+                            date.setSeconds(0)
+                            const notification = createNotificacion('DeckSwift',
+                                'Hey, do you forget to play a quiz? :(')
+                            Notifications.scheduleLocalNotificationAsync(notification, {
+                                time: date,
+                                repeat: 'day'
+                            })
+                                .then(data => setNotification({date: date.getTime(), quizDone: false}))
+                        })
+                    }
+                }
             this.setState({
                 isLoading: false,
                 store: store
@@ -40,7 +78,7 @@ export default class App extends Component {
             return (
                 <View style={styles.loading}>
                     <ActivityIndicator
-                        animating={true}
+                        animating
                         color={'green'}
                         size={'large'}
                         hidesWhenStopped={true}
--- utils/tools.js
+++ utils/tools.js
@@ -1,1 +1,17 @@
 export const capitalize = value => `${value[0].toUpperCase()}${value.slice(1).toLowerCase()}`
+
+export const createNotificacion = (title, body) => {
+    return {
+        title: title,
+        body: body,
+        ios: {
+            sound: true
+        },
+        android: {
+            sound: true,
+            priority: 'hight',
+            sticky: false,
+            vibrate: true
+        }
+    }
+}
